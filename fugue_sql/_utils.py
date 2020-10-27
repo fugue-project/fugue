eb2cbb108a74d7b8e59d48e104fd6809ec76eb80
chengxuan.wang <chengxuan.wang@cloudkitchens.com>
2020-10-27 18:19:38 +0800

    Add more tests

--- fugue_sql/_utils.py
+++ fugue_sql/_utils.py
@@ -3,22 +3,18 @@ import re
 from jinja2 import Template
 from typing import Dict, Any
 
+MATCH_QUOTED_STRING = r"([\"'])(.*?[^\\])\1"
+
 
 def fill_sql_template(sql: str, params: Dict[str, Any]):
-    i = 0
-    while i < len(sql):
-        ch = sql[i]
-        if ch == '"':
-            sql = sql[:i] + re.sub(
-                r"\"(.+?)\"", r'"{% raw %}\1{% endraw %}"', sql[i:], 1
-            )
-            i = sql.find('"', i + 1) + 1
-        elif ch == "'":
-            sql = sql[:i] + re.sub(
-                r"\'(.+?)\'", r"'{% raw %}\1{% endraw %}'", sql[i:], 1
-            )
-            i = sql.find("'", i + 1) + 1
-        else:
-            i += 1
+    single_quote_pattern = "'{{% raw %}}{}{{% endraw %}}'"
+    double_quote_pattern = '"{{% raw %}}{}{{% endraw %}}"'
+    sql = re.sub(
+        MATCH_QUOTED_STRING,
+        lambda pattern: double_quote_pattern.format(pattern.group(2))
+        if pattern.group(1) == '"'
+        else single_quote_pattern.format(pattern.group(2)),
+        sql,
+    )
     template = Template(sql)
     return template.render(**params)
--- tests/fugue_sql/test_utils.py
+++ tests/fugue_sql/test_utils.py
@@ -7,6 +7,15 @@ def test_fill_sql_template():
     assert "a=select * from b like '{%'" == fill_sql_template("a=select * from b like '{%'", data)
     assert 'a=select * from b like "{%"' == fill_sql_template('a=select * from b like "{%"', data)
     assert 'a=select * from b like "{%\'{%\'"' == fill_sql_template('a=select * from b like "{%\'{%\'"', data)
+    assert 'a=select * from b like "%}"' == fill_sql_template('a=select * from b like "%}"', data)
+    assert 'a=select * from b like "{% for env in (\'dev\', \'prod\') %}"' == fill_sql_template('a=select * from b like "{% for env in (\'dev\', \'prod\') %}"', data)
+    assert 'a=select * from b like "{% for env in (\'dev\', \'prod\') %}{% endfor %}"' == fill_sql_template('a=select * from b like "{% for env in (\'dev\', \'prod\') %}{% endfor %}"', data)
+
+    assert "a=select * from b like '{%'" == fill_sql_template("a=select * from b like '{%'", data)
+    assert "a=select * from b like '%}'" == fill_sql_template("a=select * from b like '%}'", data)
+    assert "a=select * from b like '{% for env in (\'dev\', \'prod\') %}'" == fill_sql_template("a=select * from b like '{% for env in (\'dev\', \'prod\') %}'", data)
+    assert "a=select * from b like '{% for env in (\'dev\', \'prod\') %}{% endfor %}'" == fill_sql_template("a=select * from b like '{% for env in (\'dev\', \'prod\') %}{% endfor %}'", data)    
+
     assert "1x1" == fill_sql_template("{{a}}{{b}}{{a}}", data)
     assert "" == fill_sql_template("", data)
     assert "%s" == fill_sql_template("%s", data)
