1f9bf9b271d5bbf7b6ffaafb503e5f44cb26c358
WangCHX <wcxzjtz@gmail.com>
2020-10-28 11:42:06 +0800

    Fix jinja render with keyword in template (#85)
    * Fix jinja render with keyword in template

--- fugue_sql/_utils.py
+++ fugue_sql/_utils.py
@@ -1,7 +1,20 @@
+import re
+
 from jinja2 import Template
 from typing import Dict, Any
 
+MATCH_QUOTED_STRING = r"([\"'])(.*?[^\\])\1"
+
 
 def fill_sql_template(sql: str, params: Dict[str, Any]):
+    single_quote_pattern = "'{{% raw %}}{}{{% endraw %}}'"
+    double_quote_pattern = '"{{% raw %}}{}{{% endraw %}}"'
+    sql = re.sub(
+        MATCH_QUOTED_STRING,
+        lambda pattern: double_quote_pattern.format(pattern.group(2))
+        if pattern.group(1) == '"'
+        else single_quote_pattern.format(pattern.group(2)),
+        sql,
+    )
     template = Template(sql)
     return template.render(**params)
