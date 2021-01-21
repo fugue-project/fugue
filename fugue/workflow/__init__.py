1607e3d58d4ac1f015e2f1635202d60122b9cd17
Han Wang <goodwanghan@gmail.com>
2021-01-21 20:12:35 +0000

    add df type handling

--- fugue/workflow/__init__.py
+++ fugue/workflow/__init__.py
@@ -2,4 +2,5 @@
 
 from fugue.workflow._workflow_context import FugueWorkflowContext
 from fugue.workflow.module import module
+from fugue.workflow.utils import register_raw_df_type, is_acceptable_raw_df
 from fugue.workflow.workflow import FugueWorkflow, WorkflowDataFrame, WorkflowDataFrames
