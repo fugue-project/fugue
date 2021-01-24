8e94152299311dd4895c5730aa027328e4f9c2c1
Megan Yow <Megan.yow@outlook.com>
2021-01-24 03:40:45 +0000

    updated regex

--- fugue_sql/_utils.py
+++ fugue_sql/_utils.py
@@ -1,9 +1,10 @@
 import re
+import jinja2
 
 from jinja2 import Template
 from typing import Dict, Any
 
-MATCH_QUOTED_STRING = r"([\"'])(.*?[%].*?)\1"
+MATCH_QUOTED_STRING = r"([\"'])(({|%|})*)\1"
 
 
 def fill_sql_template(sql: str, params: Dict[str, Any]):
@@ -12,16 +13,23 @@ def fill_sql_template(sql: str, params: Dict[str, Any]):
     :param sql: jinja compatible template
     :param params: params to be inserted into template
     """
-    if "self" in params:
-        params = {k: v for k, v in params.items() if k != "self"}
-    single_quote_pattern = "'{{% raw %}}{}{{% endraw %}}'"
-    double_quote_pattern = '"{{% raw %}}{}{{% endraw %}}"'
-    sql = re.sub(
-        MATCH_QUOTED_STRING,
-        lambda pattern: double_quote_pattern.format(pattern.group(2))
-        if pattern.group(1) == '"'
-        else single_quote_pattern.format(pattern.group(2)),
-        sql,
-    )
-    template = Template(sql)
+    try:
+        if "self" in params:
+            params = {k: v for k, v in params.items() if k != "self"}
+        single_quote_pattern = "'{{% raw %}}{}{{% endraw %}}'"
+        double_quote_pattern = '"{{% raw %}}{}{{% endraw %}}"'
+        new_sql = re.sub(
+            MATCH_QUOTED_STRING,
+            lambda pattern: double_quote_pattern.format(pattern.group(2))
+            if pattern.group(1) == '"'
+            else single_quote_pattern.format(pattern.group(2)),
+            sql,
+        )
+
+        template = Template(new_sql)
+
+    except jinja2.exceptions.TemplateSyntaxError:
+
+        template = Template(sql)
+
     return template.render(**params)
--- tests/fugue_sql/test_utils.py
+++ tests/fugue_sql/test_utils.py
@@ -9,51 +9,39 @@ def test_fill_sql_template():
     assert ("""select * from tbl where a = 1 and b = "x" """ 
     == fill_sql_template("""select * from tbl where a = {{a}} and b = "{{b}}" """, data)
     )
-    assert "a=select " == fill_sql_template("a=select ", data)
-    assert "a=select * from b like '{%'" == fill_sql_template(
-        "a=select * from b like '{%'", data
-    )
-    assert 'a=select * from b like "{%"' == fill_sql_template(
-        'a=select * from b like "{%"', data
-    )
-    assert "a=select * from b like \"{%'{%'\"" == fill_sql_template(
-        "a=select * from b like \"{%'{%'\"", data
-    )
-    assert 'a=select * from b like "%}"' == fill_sql_template(
-        'a=select * from b like "%}"', data
-    )
-    assert (
-        "a=select * from b like \"{% for env in ('dev', 'prod') %}\""
-        == fill_sql_template(
-            "a=select * from b like \"{% for env in ('dev', 'prod') %}\"", data
-        )
-    )
-    assert (
-        "a=select * from b like \"{% for env in ('dev', 'prod') %}{% endfor %}\""
-        == fill_sql_template(
-            "a=select * from b like \"{% for env in ('dev', 'prod') %}{% endfor %}\"",
-            data,
-        )
-    )
 
+    assert ( """select * where b="%x" """
+    == fill_sql_template("""select * where b="%{{b}}" """, data))
+    assert ( """select * where b="x%" """ 
+    == fill_sql_template("""select * where b="{{b}}%" """, data))
+
+    assert ( """select * b like "{}%{}" """
+     == fill_sql_template("""select * b like "{}%{}" """, data))
+    assert ( """select * b like '%}' """
+    == fill_sql_template("""select * b like '%}' """, data))
+    assert ("""select * where b="%x" """ 
+    == fill_sql_template("""select * where b="%{{b}}" """, data)
+    )
+    assert ("""select * where b="x%" """ 
+    == fill_sql_template("""select * where b="{{b}}%" """, data)
+    )
+    
+    assert "a=select " == fill_sql_template("a=select ", data)
+    
+    # try single quotes for finding json patterns
     assert "a=select * from b like '{%'" == fill_sql_template(
         "a=select * from b like '{%'", data
     )
     assert "a=select * from b like '%}'" == fill_sql_template(
         "a=select * from b like '%}'", data
     )
-    assert (
-        "a=select * from b like '{% for env in ('dev', 'prod') %}'"
-        == fill_sql_template(
-            "a=select * from b like '{% for env in ('dev', 'prod') %}'", data
-        )
+
+    # try double quotes for finding json patterns
+    assert 'a=select * from b like "%}"' == fill_sql_template(
+        'a=select * from b like "%}"', data
     )
-    assert (
-        "a=select * from b like '{% for env in ('dev', 'prod') %}{% endfor %}'"
-        == fill_sql_template(
-            "a=select * from b like '{% for env in ('dev', 'prod') %}{% endfor %}'",
-            data,
-        )
+    assert 'a=select * from b like "{%"' == fill_sql_template(
+        'a=select * from b like "{%"', data
     )
 
     assert "1x1" == fill_sql_template("{{a}}{{b}}{{a}}", data)
@@ -61,8 +49,8 @@ def test_fill_sql_template():
     assert "%s" == fill_sql_template("%s", data)
     assert "%%s" == fill_sql_template("%%s", data)
     assert "1%%sx1" == fill_sql_template("{{a}}%%s{{b}}{{a}}", data)
-
     assert "1" == fill_sql_template("{{a}}", {"a": 1, "self": 2})
+
 
 def test_fill_sql_template_array():
     data = {"a": [0,1,2]}
