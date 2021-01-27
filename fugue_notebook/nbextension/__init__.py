a3bb1cd4d011d2b80962b1aeda26aaeb7648f7b6
Han Wang <goodwanghan@gmail.com>
2021-01-27 09:20:13 -0800

    direct setup inside notebook (#161)
    * fugue_notebook
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update notebook test
    
    * update
    
    * add direct setup method inside notebook
    
    * update

--- fugue_notebook/nbextension/__init__.py
+++
@@ -1,9 +0,0 @@
-def _jupyter_nbextension_paths():
-    return [
-        {
-            "section": "notebook",
-            "src": "nbextension",
-            "dest": "fugue_notebook",
-            "require": "fugue_notebook/main",
-        }
-    ]
