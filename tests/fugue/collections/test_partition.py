73eee77097b703c8ead658ba704463da0a3cae8d
gityow <megyow93@gmail.com>
2021-01-31 16:49:23 +0000

    update tests

--- tests/fugue/collections/test_partition.py
+++ tests/fugue/collections/test_partition.py
@@ -1,21 +1,22 @@
 import json
 
-from fugue.collections.partition import PartitionSpec
+from fugue.collections.partition import parse_presort_exp, PartitionSpec
 from fugue.constants import KEYWORD_CORECOUNT, KEYWORD_ROWCOUNT
 from pytest import raises
 from triad.collections.schema import Schema
 from triad.utils.hash import to_uuid
+from triad.collections.dict import IndexedOrderedDict
 
 def test_parse_presort_exp():
 
-    assert parse_presort_exp() == IndexedOrderedDict()
+    assert parse_presort_exp(None) == IndexedOrderedDict()
     assert parse_presort_exp("c") == IndexedOrderedDict([('c', True)])
     assert parse_presort_exp("         c") == IndexedOrderedDict([('c', True)])
     assert parse_presort_exp("c           desc")  == IndexedOrderedDict([('c', False)])
     assert parse_presort_exp("DESC DESC, ASC ASC") == IndexedOrderedDict([('DESC', False), ('ASC', True)])
     assert parse_presort_exp("B DESC, C ASC")  == IndexedOrderedDict([('B', False), ('C', True)])
     assert parse_presort_exp("b desc, c asc") == IndexedOrderedDict([('b', False), ('c', True)])
-    assert parse_presort_exp([("b", "desc"),("c", "asc")]) == IndexedOrderedDict([('b', False), ('c', True)])
+    
 
     with raises(SyntaxError):
         parse_presort_exp("b dsc, c asc")
@@ -25,6 +26,9 @@ def test_parse_presort_exp():
 
     with raises(SyntaxError):
         parse_presort_exp("c true, c true")
+
+    with raises(SyntaxError):
+        parse_presort_exp([("b", "desc"),("c", "asc")])
 
 
 def test_partition_spec():
