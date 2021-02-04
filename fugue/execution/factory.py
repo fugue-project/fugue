33d0fb4a66786b05b1d7ff0a22610575d8e68be3
Han Wang <goodwanghan@gmail.com>
2021-02-04 06:12:17 +0000

    update comments

--- fugue/execution/factory.py
+++ fugue/execution/factory.py
@@ -104,24 +104,211 @@ _EXECUTION_ENGINE_FACTORY = _ExecutionEngineFactory()
 
 
 def register_execution_engine(name: str, func: Callable, on_dup="overwrite") -> None:
+    """Register :class:`~fugue.execution.execution_engine.ExecutionEngine` with
+    a given name.
+
+    :param name: name of the execution engine
+    :param func: a callable taking |ParamsLikeObject| and ``**kwargs`` and returning an
+      :class:`~fugue.execution.execution_engine.ExecutionEngine` instance
+    :param on_dup: action on duplicated ``name``. It can be "overwrite", "ignore"
+      (not overwriting) or "throw" (throw exception), defaults to "overwrite".
+
+    :raises KeyError: if ``on_dup`` is ``throw`` and the ``name`` already exists
+
+    :Example:
+
+    .. code-block:: python
+
+        # create a new engine with name my (overwrites if existed)
+        register_execution_engine("my", lambda conf: MyExecutionEngine(conf))
+
+        # 0
+        make_execution_engine("my")
+        make_execution_engine("my", {"myconfig":"value})
+
+        # 1
+        with FugueWorkflow("my") as dag:
+            dag.create([[0]],"a:int").show()
+
+        # 2
+        dag = FugueWorkflow()
+        dag.create([[0]],"a:int").show()
+        dag.run("my", {"myconfig":"value})
+
+        # 3
+        fsql('''
+        CREATE [[0]] SCHEMA a:int
+        PRINT
+        ''').run("my")
+    """
     _EXECUTION_ENGINE_FACTORY.register(name, func, on_dup)
 
 
 def register_default_execution_engine(func: Callable, on_dup="overwrite") -> None:
+    """Register :class:`~fugue.execution.execution_engine.ExecutionEngine` as the
+    default engine.
+
+    :param func: a callable taking |ParamsLikeObject| and ``**kwargs`` and returning an
+      :class:`~fugue.execution.execution_engine.ExecutionEngine` instance
+    :param on_dup: action on duplicated ``name``. It can be "overwrite", "ignore"
+      (not overwriting) or "throw" (throw exception), defaults to "overwrite".
+
+    :raises KeyError: if ``on_dup`` is ``throw`` and the ``name`` already exists
+
+    :Example:
+
+    .. code-block:: python
+
+        # create a new engine with name my (overwrites if existed)
+        register_default_execution_engine(lambda conf: MyExecutionEngine(conf))
+
+        # the following examples will use MyExecutionEngine
+
+        # 0
+        make_execution_engine()
+        make_execution_engine(None, {"myconfig":"value})
+
+        # 1
+        with FugueWorkflow() as dag:
+            dag.create([[0]],"a:int").show()
+
+        # 2
+        dag = FugueWorkflow()
+        dag.create([[0]],"a:int").show()
+        dag.run(None, {"myconfig":"value})
+
+        # 3
+        fsql('''
+        CREATE [[0]] SCHEMA a:int
+        PRINT
+        ''').run("", {"myconfig":"value})
+    """
     _EXECUTION_ENGINE_FACTORY.register_default(func, on_dup)
 
 
 def register_sql_engine(name: str, func: Callable, on_dup="overwrite") -> None:
+    """Register :class:`~fugue.execution.execution_engine.SQLEngine` with
+    a given name.
+
+    :param name: name of the SQL engine
+    :param func: a callable taking
+      :class:`~fugue.execution.execution_engine.ExecutionEngine`
+      and ``**kwargs`` and returning a
+      :class:`~fugue.execution.execution_engine.SQLEngine` instance
+    :param on_dup: action on duplicated ``name``. It can be "overwrite", "ignore"
+      (not overwriting) or "throw" (throw exception), defaults to "overwrite".
+
+    :raises KeyError: if ``on_dup`` is ``throw`` and the ``name`` already exists
+
+    :Example:
+
+    .. code-block:: python
+
+        # create a new engine with name my (overwrites if existed)
+        register_sql_engine("mysql", lambda engine: MySQLEngine(engine))
+
+        # create execution engine with MySQLEngine as the default
+        make_execution_engine(("", "mysql"))
+
+        # create DaskExecutionEngine with MySQLEngine as the default
+        make_execution_engine(("dask", "mysql"))
+
+        # default execution engine + MySQLEngine
+        with FugueWorkflow(("","mysql")) as dag:
+            dag.create([[0]],"a:int").show()
+    """
     _EXECUTION_ENGINE_FACTORY.register_sql_engine(name, func, on_dup)
 
 
 def register_default_sql_engine(func: Callable, on_dup="overwrite") -> None:
+    """Register :class:`~fugue.execution.execution_engine.SQLEngine` as the
+    default engine
+
+    :param func: a callable taking
+      :class:`~fugue.execution.execution_engine.ExecutionEngine`
+      and ``**kwargs`` and returning a
+      :class:`~fugue.execution.execution_engine.SQLEngine` instance
+    :param on_dup: action on duplicated ``name``. It can be "overwrite", "ignore"
+      (not overwriting) or "throw" (throw exception), defaults to "overwrite".
+
+    :raises KeyError: if ``on_dup`` is ``throw`` and the ``name`` already exists
+
+    :Notice:
+    You should be careful to use this function, because when you set a custom
+    SQL engine as default, all execution engines you create will use this SQL
+    engine unless you are explicit. For example if you set the default SQL engine
+    to be a Spark specific one, then if you start a NativeExecutionEngine, it will
+    try to use it and will throw exceptions.
+
+    So it's always a better idea to use ``register_sql_engine`` instead
+
+    :Example:
+
+    .. code-block:: python
+
+        # create a new engine with name my (overwrites if existed)
+        register_default_sql_engine(lambda engine: MySQLEngine(engine))
+
+        # create NativeExecutionEngine with MySQLEngine as the default
+        make_execution_engine()
+
+        # create SparkExecutionEngine with MySQLEngine instead of SparkSQLEngine
+        make_execution_engine("spark")
+
+        # NativeExecutionEngine with MySQLEngine
+        with FugueWorkflow() as dag:
+            dag.create([[0]],"a:int").show()
+    """
     _EXECUTION_ENGINE_FACTORY.register_default_sql_engine(func, on_dup)
 
 
 def make_execution_engine(
     engine: Any = None, conf: Any = None, **kwargs: Any
 ) -> ExecutionEngine:
+    """Create :class:`~fugue.execution.execution_engine.ExecutionEngine`
+    with specified ``engine``
+
+    :param engine: it can be empty string or null (use the default execution
+      engine), a string (use the registered execution engine), an
+      :class:`~fugue.execution.execution_engine.ExecutionEngine` type, or
+      the :class:`~fugue.execution.execution_engine.ExecutionEngine` instance
+      , or a tuple of two values where the first value represents execution
+      engine and the second value represents the sql engine (you can use ``None``
+      for either of them to use the default one), defaults to None
+    :param conf: |ParamsLikeObject|, defaults to None
+    :param kwargs: additional parameters to initialize the execution engine
+
+    :return: the :class:`~fugue.execution.execution_engine.ExecutionEngine`
+      instance
+
+    :Example:
+
+    .. code-block:: python
+
+        register_default_execution_engine(lambda conf: E1(conf))
+        register_execution_engine("e2", lambda conf, **kwargs: E2(conf, **kwargs))
+
+        register_sql_engine("s", lambda conf: S2(conf))
+
+        # E1 + E1.default_sql_engine
+        make_execution_engine()
+
+        # E2 + E2.default_sql_engine
+        make_execution_engine(e2)
+
+        # E1 + S2
+        make_execution_engine((None, "s"))
+
+        # E2(conf, a=1, b=2) + S2
+        make_execution_engine(("e2", "s"), conf, a=1, b=2)
+
+        # SparkExecutionEngine + SparkSQLEngine
+        make_execution_engine(SparkExecutionEngine)
+        make_execution_engine(SparkExecutionEngine(spark_session, conf))
+
+        # SparkExecutionEngine + S2
+        make_execution_engine((SparkExecutionEngine, "s"))
+    """
     return _EXECUTION_ENGINE_FACTORY.make(engine, conf, **kwargs)
 
 
@@ -130,4 +317,46 @@ def make_sql_engine(
     execution_engine: Optional[ExecutionEngine] = None,
     **kwargs: Any,
 ) -> SQLEngine:
+    """Create :class:`~fugue.execution.execution_engine.SQLEngine`
+    with specified ``engine``
+
+    :param engine: it can be empty string or null (use the default SQL
+      engine), a string (use the registered SQL engine), an
+      :class:`~fugue.execution.execution_engine.SQLEngine` type, or
+      the :class:`~fugue.execution.execution_engine.SQLEngine` instance
+      (you can use ``None`` to use the default one), defaults to None
+    :param execution_engine: the
+      :class:`~fugue.execution.execution_engine.ExecutionEngine` instance
+      to create
+      the :class:`~fugue.execution.execution_engine.SQLEngine`. Normally you
+      should always provide this value.
+    :param kwargs: additional parameters to initialize the sql engine
+
+    :return: the :class:`~fugue.execution.execution_engine.SQLEngine`
+      instance
+
+    :Notice:
+    For users, you normally don't need to call this function directly.
+    Use ``make_execution_engine`` instead
+
+    :Example:
+
+    .. code-block:: python
+        register_default_sql_engine(lambda conf: S1(conf))
+        register_sql_engine("s2", lambda conf: S2(conf))
+
+        engine = NativeExecutionEngine()
+
+        # S1(engine)
+        make_sql_engine(None, engine)
+
+        # S1(engine, a=1)
+        make_sql_engine(None, engine, a=1)
+
+        # S2(engine)
+        make_sql_engine("s2", engine)
+
+        # SqliteEngine(engine)
+        make_sql_engine(SqliteEngine)
+    """
     return _EXECUTION_ENGINE_FACTORY.make_sql_engine(engine, execution_engine, **kwargs)
