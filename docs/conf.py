6c34e71a5f438324cc3bcfcc93564f428e330657
Han Wang <goodwanghan@gmail.com>
2021-02-15 08:22:20 +0000

    doc logo

--- docs/conf.py
+++ docs/conf.py
@@ -63,6 +63,12 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_logo = "_static/logo_doc.svg"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': True,
+    'style_nav_header_background': '#264263'
+}
 
 master_doc = "index"
 
