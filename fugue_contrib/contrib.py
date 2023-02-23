1629224f8e85f78e07721b97aee32b427e9a86c7
Jamie Broomall <jamie@whylabs.ai>
2023-02-23 11:42:42 -0800

    Register whylogs extension

--- fugue_contrib/contrib.py
+++ fugue_contrib/contrib.py
@@ -3,4 +3,5 @@ from typing import Dict, Any
 FUGUE_CONTRIB: Dict[str, Any] = {
     "viz": {"module": "fugue_contrib.viz"},
     "sns": {"module": "fugue_contrib.seaborn"},
+    "why": {"module": "whylogs.api.fugue.registry"},
 }
