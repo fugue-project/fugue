b4e4fa28cc2c217295db4c20e31bc98802f35b02
Ron Bergeron <RBergeron@users.noreply.github.com>
2023-03-09 15:49:15 -0500

    Updated outtransform docs (#437)
    * Updated outtransform docs
    
    * Updated supported file formats in docs

--- fugue/workflow/api.py
+++ fugue/workflow/api.py
@@ -196,7 +196,7 @@ def out_transform(
 ) -> None:
     """Transform this dataframe using transformer. It's a wrapper of
     :meth:`~fugue.workflow.workflow.FugueWorkflow.out_transform` and
-    :meth:`~fugue.workflow.workflow.FugueWorkflow.run`. It let you do the
+    :meth:`~fugue.workflow.workflow.FugueWorkflow.run`. It will let you do the
     basic dataframe transformation without using
     :class:`~fugue.workflow.workflow.FugueWorkflow` and
     :class:`~fugue.dataframe.dataframe.DataFrame`. Only native types are
@@ -207,10 +207,10 @@ def out_transform(
     :param df: |DataFrameLikeObject| or :class:`~fugue.workflow.yielded.Yielded`
         or a path string to a parquet file
     :param using: transformer-like object, can't be a string expression
-    :param params: |ParamsLikeObject| to run the processor, defaults to None.
+    :param params: |ParamsLikeObject| to run the processor, defaults to None
         The transformer will be able to access this value from
         :meth:`~fugue.extensions.context.ExtensionContext.params`
-    :param partition: |PartitionLikeObject|, defaults to None.
+    :param partition: |PartitionLikeObject|, defaults to None
     :param callback: |RPCHandlerLikeObject|, defaults to None
     :param ignore_errors: list of exception types the transformer can ignore,
         defaults to None (empty list)
@@ -225,7 +225,7 @@ def out_transform(
 
     .. note::
 
-        This function can only take parquet file paths in `df`. Csv and other file
+        This function can only take parquet file paths in `df`. CSV and JSON file
         formats are disallowed.
 
         This transformation is guaranteed to execute immediately (eager)
