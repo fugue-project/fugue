88551ef58ac2a8314fe1a8c2a24a44c1efca7dab
David Vegh <61405792+veghdev@users.noreply.github.com>
2023-03-16 09:59:53 +0100

    contrib: register the vizzu extension

--- fugue_contrib/contrib.py
+++ fugue_contrib/contrib.py
@@ -4,4 +4,5 @@ FUGUE_CONTRIB: Dict[str, Any] = {
     "viz": {"module": "fugue_contrib.viz"},
     "sns": {"module": "fugue_contrib.seaborn"},
     "why": {"module": "whylogs.api.fugue.registry"},
+    "vizzu": {"module": "ipyvizzu.integrations.fugue"},
 }
