5e0a0c619f14bf5e805c0d24ba3606d1552ddc5e
Han Wang <goodwanghan@gmail.com>
2024-04-25 06:11:49 +0000

    update

--- scripts/setupsparkconnect.sh
+++ scripts/setupsparkconnect.sh
@@ -1,3 +1,3 @@
 wget https://dlcdn.apache.org/spark/spark-3.5.1/spark-3.5.1-bin-hadoop3.tgz -O - | tar -xz -C /tmp
 # export SPARK_NO_DAEMONIZE=1
-bash /tmp/spark-3.5.1-bin-hadoop3/sbin/start-connect-server.sh --jars https://repo1.maven.org/maven2/org/apache/spark/spark-connect_2.12/3.5.1/spark-connect_2.12-3.5.0.jar
+bash /tmp/spark-3.5.1-bin-hadoop3/sbin/start-connect-server.sh --jars https://repo1.maven.org/maven2/org/apache/spark/spark-connect_2.12/3.5.1/spark-connect_2.12-3.5.1.jar
