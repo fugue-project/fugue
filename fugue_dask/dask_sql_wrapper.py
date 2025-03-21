8c6a91e45e0b7895500282c4d910d9771ce04deb
Han Wang <han@Hans-MacBook-Pro.local>
2025-03-21 00:11:30 -0700

    update

--- fugue_dask/dask_sql_wrapper.py
+++ fugue_dask/dask_sql_wrapper.py
@@ -1,7 +1,14 @@
 from typing import Any, Optional
 
 import dask.dataframe as dd
-from dask.dataframe.dask_expr.io.parquet import ReadParquet
+
+try:
+    from dask.dataframe.dask_expr.io.parquet import ReadParquet
+
+    HAS_DASK_EXPR = True
+except ImportError:  # pragma: no cover
+    HAS_DASK_EXPR = False
+
 from triad.utils.assertion import assert_or_throw
 
 try:
@@ -13,50 +20,53 @@ except ImportError:  # pragma: no cover
         "dask-sql is not installed. Please install it with `pip install dask-sql`"
     )
 
+if not HAS_DASK_EXPR:  # pragma: no cover
+    ContextWrapper = Context
+else:
 
-class ContextWrapper(Context):
-    def create_table(
-        self,
-        table_name: str,
-        input_table: dd.DataFrame,
-        format: Optional[str] = None,  # noqa
-        persist: bool = False,
-        schema_name: Optional[str] = None,
-        statistics: Optional[Statistics] = None,
-        gpu: bool = False,
-        **kwargs: Any,
-    ) -> None:
-        assert_or_throw(
-            isinstance(input_table, dd.DataFrame),
-            lambda: ValueError(
-                f"input_table must be a dask dataframe, but got {type(input_table)}"
-            ),
-        )
-        assert_or_throw(
-            dd._dask_expr_enabled(), lambda: ValueError("Dask expr must be enabled")
-        )
-        schema_name = schema_name or self.schema_name
+    class ContextWrapper(Context):
+        def create_table(
+            self,
+            table_name: str,
+            input_table: dd.DataFrame,
+            format: Optional[str] = None,  # noqa
+            persist: bool = False,
+            schema_name: Optional[str] = None,
+            statistics: Optional[Statistics] = None,
+            gpu: bool = False,
+            **kwargs: Any,
+        ) -> None:
+            assert_or_throw(
+                isinstance(input_table, dd.DataFrame),
+                lambda: ValueError(
+                    f"input_table must be a dask dataframe, but got {type(input_table)}"
+                ),
+            )
+            assert_or_throw(
+                dd._dask_expr_enabled(), lambda: ValueError("Dask expr must be enabled")
+            )
+            schema_name = schema_name or self.schema_name
 
-        dc = InputUtil.to_dc(
-            input_table,
-            table_name=table_name,
-            format=format,
-            persist=persist,
-            gpu=gpu,
-            **kwargs,
-        )
+            dc = InputUtil.to_dc(
+                input_table,
+                table_name=table_name,
+                format=format,
+                persist=persist,
+                gpu=gpu,
+                **kwargs,
+            )
 
-        dask_filepath = None
-        operations = input_table.find_operations(ReadParquet)
-        for op in operations:
-            dask_filepath = op._args[0]
+            dask_filepath = None
+            operations = input_table.find_operations(ReadParquet)
+            for op in operations:
+                dask_filepath = op._args[0]
 
-        dc.filepath = dask_filepath
-        self.schema[schema_name].filepaths[table_name.lower()] = dask_filepath
+            dc.filepath = dask_filepath
+            self.schema[schema_name].filepaths[table_name.lower()] = dask_filepath
 
-        if not statistics:
-            statistics = Statistics(float("nan"))
-        dc.statistics = statistics
+            if not statistics:
+                statistics = Statistics(float("nan"))
+            dc.statistics = statistics
 
-        self.schema[schema_name].tables[table_name.lower()] = dc
-        self.schema[schema_name].statistics[table_name.lower()] = statistics
+            self.schema[schema_name].tables[table_name.lower()] = dc
+            self.schema[schema_name].statistics[table_name.lower()] = statistics
--- scripts/setupsparkconnect.sh
+++ scripts/setupsparkconnect.sh
@@ -1,3 +1,3 @@
-wget https://dlcdn.apache.org/spark/spark-3.5.2/spark-3.5.2-bin-hadoop3.tgz -O - | tar -xz -C /tmp
+wget https://dlcdn.apache.org/spark/spark-3.5.5/spark-3.5.5-bin-hadoop3.tgz -O - | tar -xz -C /tmp
 # export SPARK_NO_DAEMONIZE=1
-bash /tmp/spark-3.5.2-bin-hadoop3/sbin/start-connect-server.sh --jars https://repo1.maven.org/maven2/org/apache/spark/spark-connect_2.12/3.5.2/spark-connect_2.12-3.5.2.jar
+bash /tmp/spark-3.5.5-bin-hadoop3/sbin/start-connect-server.sh --jars https://repo1.maven.org/maven2/org/apache/spark/spark-connect_2.12/3.5.5/spark-connect_2.12-3.5.5.jar
